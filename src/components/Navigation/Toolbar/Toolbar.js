cad14304b8d0bae8e1863ee0e040ffc855b6aeaa
Shane Stephenson <shanestephenson@Shanes-MacBook-Pro.local>
2020-12-14 22:50:06 -0500

    Added media queries for mobile devices

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -7,7 +7,7 @@ const toolbar = (props) => (
   <header className={classes.Toolbar}>
     <div>MENU</div>
     <Logo height="80%" />
-    <nav>
+    <nav className={classes.DesktopOnly}>
       <NavigationItems />
     </nav>
   </header>
