78e5a5acd3589ca45b8e532a64218f2c42f54586
Shane Stephenson <shanestephenson@Shanes-MacBook-Pro.local>
2020-12-15 17:00:09 -0500

    Finishing off burger builder deisgn for mobile devices and adding lifecycle methods for performance enhancements

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,30 +1,32 @@
-import React from 'react';
-import Aux from '../../../hoc/Aux';
+import React, {Component} from 'react';
+import Aux from '../../../hoc/Aux/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+  render () {
+    const ingredientSummary = Object.keys(this.props.ingredients)
     .map(igKey => {
     return (
       <li key={igKey}>
-        <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
+        <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
       </li>
       );
     });
 
-  return (
-    <Aux>
-      <h3>Your Order</h3>
-      <p>Burger Ingredients:</p>
-      <ul>
-        {ingredientSummary}
-      </ul>
-      <strong><p>Total Price: {props.price.toFixed(2)}</p></strong>
-      <p>Continue to Checkout?</p>
-      <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-      <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-    </Aux>
-  )
+    return (
+      <Aux>
+        <h3>Your Order</h3>
+        <p>Burger Ingredients:</p>
+        <ul>
+          {ingredientSummary}
+        </ul>
+        <strong><p>Total Price: {this.props.price.toFixed(2)}</p></strong>
+        <p>Continue to Checkout?</p>
+        <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+        <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+      </Aux>
+    )
+  }
 };
 
-export default orderSummary;
+export default OrderSummary;
