1e65124cfb0c5eba1f8fb95bab76a5194133e679
Shane Stephenson <shanestephenson@Shanes-MacBook-Pro.local>
2021-01-03 15:02:51 -0500

    Additional enhancements, small design changes, lazy loading of components, utility functions

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -57,7 +57,6 @@ export const auth = (email, password, isSignUp) => {
 
     axios.post(url, authData)
       .then(response => {
-        console.log(response);
         const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
         localStorage.setItem('token', response.data.idToken);
         localStorage.setItem('expirationDate', expirationDate);
@@ -66,7 +65,6 @@ export const auth = (email, password, isSignUp) => {
         dispatch(checkAuthTimout(response.data.expiresIn));
       })
       .catch(err => {
-        console.log(err);
         dispatch(authFail(err.response.data.error));
       });
   };
