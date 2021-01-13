1c076fc19b2c5083b0c5473ae174223b61e30f02
Shane Stephenson <shanestephenson@Shanes-MacBook-Pro.local>
2021-01-12 22:42:45 -0500

    Started converting project to use React Hooks

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -5,35 +5,34 @@ import {connect} from 'react-redux';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 import ContactData from './ContactData/ContactData';
 
-class Checkout extends Component {
-  checkoutCancelledHandler = () => {
-    this.props.history.goBack();
+const checkout = props => {
+  const checkoutCancelledHandler = () => {
+    props.history.goBack();
   }
 
-  checkoutContinuedHandler = () => {
-    this.props.history.replace('/checkout/contact-data');
+  const checkoutContinuedHandler = () => {
+    props.history.replace('/checkout/contact-data');
   }
 
-  render () {
-    let summary = <Redirect to="/" />
+  let summary = <Redirect to="/" />
 
-    if(this.props.ings){
-      const purchaseRedirect = this.props.purchased ? <Redirect  to="/"/> : null;
+  if(props.ings){
+    const purchaseRedirect = props.purchased ? <Redirect  to="/"/> : null;
 
-      summary = (
-      <div>
-        {purchaseRedirect}
-         <CheckoutSummary 
-            ingredients={this.props.ings} 
-            checkoutCancelled={this.checkoutCancelledHandler} 
-            checkoutContinued={this.checkoutContinuedHandler}/>
-            <Route path={this.props.match.path + '/contact-data'} component={ContactData} />
-        </div> )
+    summary = (
+    <div>
+      {purchaseRedirect}
+        <CheckoutSummary 
+          ingredients={props.ings} 
+          checkoutCancelled={checkoutCancelledHandler} 
+          checkoutContinued={checkoutContinuedHandler}/>
+          <Route path={props.match.path + '/contact-data'} component={ContactData} />
+      </div> )
 
-    }
-      return summary;
-    }
-}
+  }
+    return summary;
+  }
+
 
 const mapStateToProps = state => {
   return {
@@ -42,4 +41,4 @@ const mapStateToProps = state => {
   }
 };
 
-export default connect(mapStateToProps)(Checkout);
+export default connect(mapStateToProps)(checkout);
--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {useEffect} from 'react';
 import {connect} from 'react-redux';
 import Order from '../../components/Order/Order';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
@@ -6,29 +6,27 @@ import * as actions from '../../store/actions/index';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import axios from '../../axios-orders';
 
-class Orders extends Component {
-  componentDidMount(){
-    this.props.onFetchOrders(this.props.token, this.props.userId);
+const orders = props => {
+  useEffect(() => {
+    props.onFetchOrders(props.token, props.userId);
+  }, []);
+
+  let orders = <Spinner />;
+
+  if(!props.loading) {
+    orders = props.orders.map(order => (
+      <Order 
+        key={order.id}
+        ingredients={order.ingredients}
+        price={+order.price}
+        />
+    ))
   }
-
-  render () {
-    let orders = <Spinner />;
-
-    if(!this.props.loading) {
-      orders = this.props.orders.map(order => (
-        <Order 
-          key={order.id}
-          ingredients={order.ingredients}
-          price={+order.price}
-          />
-      ))
-    }
-    return (
-      <div>
-        {orders}
-      </div>  
-    );
-  } 
+  return (
+    <div>
+      {orders}
+    </div>  
+  );
 }
 
 const mapStateToProps = state => {
@@ -46,4 +44,4 @@ const mapDispatchToProps = dispatch => {
   };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(Orders, axios));
+export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(orders, axios));
